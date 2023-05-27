1ac37282c0c2baf7685ca04e5accf0b8c46eb9c5
moses gitonga <infosec947@fmail.com>
2023-05-27 13:03:58 +0300

    added some conditoins in collection of data

--- collect_data.c
+++ collect_data.c
@@ -8,22 +8,49 @@ void collect_data(voters_data *data)
 
 	data->location = malloc(sizeof(char) * 100);
 
+
        	printf("Enter your First Name: \n");
 	fgets(data->name, 100, stdin);
 	data->name[strcspn(data->name, "\n")] = '\0';
+	for (int i = 0; data->name[i] != '\0'; i++)
+	{
+		if (isdigit(data->name[i]))
+		{
+			fprintf(stderr, "No digits should be in First Name\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+
 
 	printf("Enter your Second Name: \n");
        	fgets(data->lname, 100, stdin);
 	data->lname[strcspn(data->lname, "\n")] = '\0';
+	for (int i = 0; data->lname[i] != '\0'; i++)
+	{
+		if (isdigit(data->lname[i]))
+		{
+			fprintf(stderr, "No digits should be in the second name\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
 
 	printf("Enter Your Age: \n");
 	scanf("%d", &(data->Age));
+	if (data->Age < 18)
+	{
+		fprintf(stderr, "Unable  to register,\n\tThis person is considerd an infant\n");
+		exit(EXIT_FAILURE);
+	}
 	getchar();
 
 	printf("Enter Your Location: \n");
 	fgets(data->location, 100, stdin);
 	data->location[strcspn(data->location, "\n")] = '\0';
-	printf("Enter your id Number");
+	
+	printf("Enter your id Number: \n");
 	scanf("%d", &(data->id));
+	getchar();
 
 }
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdlib.h>
