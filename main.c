23a8e29c295dc9f1dc3d66e52c41e79a46871205
moses gitonga <infosec947@fmail.com>
2023-05-28 14:19:50 +0300

    updated the search function

--- main.c
+++ main.c
@@ -4,14 +4,14 @@ int main()
 {
 	voters_data data;
 	int a;
-	printf("Enter 1 to collect data OR enter 2 search name");
+	printf("Enter\n1 to collect data\n2 to search name:\n");
 	scanf("%d", &a);
 	getchar();
 	if (a == 1)
 	{
 	
 		FILE *file_name;
-		file_name = fopen("voter_data_storage.txt", "a");
+		file_name = fopen("voter data storage.txt", "a");
 		if (file_name == NULL)
 		{
 			fprintf(stderr, "unable to open file");
--- search.c
+++ search.c
@@ -16,6 +16,7 @@ void search_voter(voters_data *data)
 	{
 		fprintf(stderr, "unable to open file\n\tfile might be empty");
 	}
+	int found = 0;
 	while  (fgets(line, sizeof(line), file))
 	{
 		char name[100];
@@ -24,14 +25,14 @@ void search_voter(voters_data *data)
 		char location[100];
 		int id;
 
-		if (sscanf(line, "Name: %99s %99s", name, lname) == 2)
+		if (sscanf(line, "Name: %s %s", name, lname) == 2)
 		{
 			if (strcmp(name, search_name) == 0)
 			{
-				printf("Name: %s %s", name, lname);
+				printf("Name: %s %s\n", name, lname);
 				
 				fgets(line, sizeof(line), file);
-				sscanf(line, "Age %d", &Age);
+				sscanf(line, "Age: %d", &Age);
 				printf("Age: %d\n", Age);
 
 				fgets(line,sizeof(line), file);
@@ -39,12 +40,19 @@ void search_voter(voters_data *data)
 				printf("Location: %s\n", location);
 				
 				fgets(line, sizeof(line), file);
-				sscanf(line, "ID Number %d", &id);
+				sscanf(line, "ID Number: %99d", &id);
 				printf("ID Number: %d\n", id);				
 
-				break;
+				found = 1;
+			
 			}
 		}
 	}
+	if (!found)
+	{
+		fprintf(stderr, "Name '%s' is not registered.\n", search_name);
+	}
+
+
 	fclose(file);
 }
